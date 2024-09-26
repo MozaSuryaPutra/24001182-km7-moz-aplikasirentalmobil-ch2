706a42651d4d5c9d884722376c9b04766a3a7a65
MozaSuryaPutra <mozasuryaputra@gmail.com>
2024-09-26 23:04:36 +0700

    Hampir bisa

--- script/forcar.js
+++ script/forcar.js
@@ -14,8 +14,8 @@ function randomDate(start, end, startHour, endHour) {
 async function carDate() {
   const datum = await getCarsData();
   datum.forEach((cars) => {
-    const start = new Date(2022, 0, 1);
-    const end = new Date();
+    const start = new Date(2024, 8, 26);
+    const end = new Date(2024, 9, 11);
     const startHour = 0;
     const endHour = 23;
 
@@ -24,32 +24,132 @@ async function carDate() {
   return datum;
 }
 
-async function updateData() {
+async function updateData(driver, date, passenger) {
+  const isWithDriver = driver === "denganSopir";
+  const driverValue = isWithDriver ? true : false;
+
   const dataout = await carDate();
-  return dataout;
+  const filteredData = dataout.filter((car) => {
+    return (
+      car.available === driverValue &&
+      car.availableAt >= date &&
+      car.capacity >= passenger
+    );
+  });
+  return filteredData;
 }
+
+const carContentElement = document.getElementById("car-content");
 
 document.getElementById("submit").addEventListener("click", async (event) => {
   event.preventDefault();
   const dateInputValue = document.getElementById("dateInput").value;
+  const driverInputValue = document.getElementById("driverInput").value;
+  const passengerInput = document.getElementById("passengerInput").value;
 
-  if (dateInputValue === "") {
-    console.log("Input tanggal kosong. Tidak ada tindakan yang diambil.");
+  if (dateInputValue === "" || driverInputValue === "") {
+    console.log(
+      "Input tanggal atau driver kosong. Tidak ada tindakan yang diambil."
+    );
     return;
   }
 
-  const dateInput = new Date(dateInputValue);
-  const passengerInput = document.getElementById("passengerInput").value;
+  const dateInput = new Date(dateInputValue).toISOString();
+  const RealData = await updateData(
+    driverInputValue,
+    dateInput,
+    passengerInput
+  );
 
-  const RealData = await updateData();
+  console.log(RealData);
 
+  let filteredCarsHTML = "";
   RealData.forEach((car) => {
     const carAvailableAt = new Date(car.availableAt);
-
     if (carAvailableAt > dateInput && car.capacity > passengerInput) {
-      console.log("Bisa");
-    } else {
-      console.log("Gabisa");
+      const carcontent = `
+        <div class="col-md-4 h-100 pb-2 pt-2">
+          <div class="card h-100" style="width: 18rem">
+            <div class="card-body">
+              <h5 class="card-title">${car.model}</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">
+               RP ${car.rentPerDay} / hari
+              </h6>
+              <p class="card-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              </p>
+              <p class="card-text">
+                Kapasitas: ${car.capacity} orang
+              </p>
+              <p class="card-text">
+                Manual/Automatic: ${car.transmission}
+              </p>
+              <p class="card-text">
+                Tahun : ${car.year}
+              </p>
+            </div>
+          </div>
+        </div>
+      `;
+      filteredCarsHTML += carcontent;
     }
   });
+  carContentElement.innerHTML = filteredCarsHTML;
 });
+// async function handleSubmit(event) {
+//   event.preventDefault();
+//   const dateInputValue = document.getElementById("dateInput").value;
+//   const driverInputValue = document.getElementById("driverInput").value;
+//   const passengerInputValue = document.getElementById("passengerInput").value;
+
+//   if (dateInputValue === "" || driverInputValue === "") {
+//     console.log("Input tanggal kosong. Tidak ada tindakan yang diambil.");
+//     return;
+//   } else if (dateInputValue === "" && driverInputValue === "") {
+//     console.log("Input tanggal kosong. Tidak ada tindakan yang diambil.");
+//     return;
+//   }
+
+//   const dateInput = new Date(dateInputValue).toISOString;
+
+//   const RealData = await updateData(
+//     driverInputValue,
+//     dateInput,
+//     passengerInputValue
+//   );
+
+//   console.log(RealData);
+
+//   let filteredCarsHTML = "";
+//   RealData.forEach((car) => {
+//     const carcontent = `
+//     <div class="col-md-4 h-100 pb-2 pt-2">
+//       <div class="card h-100" style="width: 18rem">
+//         <div class="card-body">
+//           <h5 class="card-title">${car.model}</h5>
+//           <h6 class="card-subtitle mb-2 text-body-secondary">
+//            RP ${car.rentPerDay} / hari
+//           </h6>
+//           <p class="card-text">
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+//           </p>
+//           <p class="card-text">
+//             Kapasitas: ${car.capacity} orang
+//           </p>
+//           <p class="card-text">
+//             Manual/Automatic: ${car.transmission}
+//           </p>
+//           <p class="card-text">
+//             Tahun : ${car.year}
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   `;
+//     filteredCarsHTML += carcontent;
+//   });
+
+//   carContentElement.innerHTML = filteredCarsHTML;
+// }
+
+// document.getElementById("submit").addEventListener("click", handleSubmit);
